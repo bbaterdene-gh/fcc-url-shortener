9132ce796c4a40af7874f21d1c2691949d1ac3b9
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-08-28 10:24:49 +0800

    post short url

--- models/short_url.js
+++ models/short_url.js
@@ -4,6 +4,13 @@ const Schema = mongoose.Schema
 const ShortURLSchema = new Schema({
   url_id: { type: Number, unique: true },
   original_url: { type: String, unique: true },
-})
+}, { collection: 'short_urls' })
+
+ShortURLSchema.statics.findMaxUrlIdAndCreate = function(callback) {
+  this.find().sort('-url_id').limit(1).exec((err, short_urls) => {
+    if (err || short_urls.length == 0) console.log(err)
+    return callback(short_urls[0].url_id)
+  })
+}
 
 module.exports = mongoose.model('ShortURL', ShortURLSchema)
--- routes/shortURL.js
+++ routes/shortURL.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { body } = require('express-validator')
+const dns = require('dns')
+const ShortURL = require('../models/short_url')
+const mongoose = require('mongoose')
+const db = mongoose.collections
 
 /* GET home page. */
 router.get('/shorturl/:url_id', (req, res, next) => {
@@ -7,8 +12,46 @@ router.get('/shorturl/:url_id', (req, res, next) => {
   res.render('index')
 })
 
-router.post('/shorturl', (req, res, next) => {
+router.post(
+  '/shorturl',
+  body('url').trim(),
+  (req, res, next) => {
+  let url = req.body.url
+  url = url.replace(/^https?:\/\//, '')
+  url = url.replace(/\/$/, '')
 
+  dns.lookup(url, (err) => {
+    if (err) {
+      return res.json({
+        error: 'Invalid URL'
+      })
+    }
+    ShortURL.findOne({ original_url: req.body.url }).exec((err, short_url) => {
+      if (err) console.error(err)
+      // already existed
+      if (short_url) {
+        return res.json({
+          short_url: short_url.url_id,
+          original_url: short_url.original_url,
+        })
+      } else {
+        // newly create
+        ShortURL.findMaxUrlIdAndCreate((maxUrlID) => {
+          ShortURL.create({
+            url_id: +maxUrlID + 1,
+            original_url: req.body.url
+          }, (err, short_url) => {
+            if(err) console.error(err)
+            return res.json({
+              short_url: short_url.url_id,
+              original_url: short_url.original_url,
+            })
+          })
+        })
+      }
+    })
+  })
 })
+
 
 module.exports = router;
